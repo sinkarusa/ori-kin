e86375683589e5c68b8b3c728f79409845defd61
sinanus <sinankaraveli@gmail.com>
2025-03-18 09:52:51 -0400

    correct implementation of overlapping trace removal. probably could use some cleaning

--- app/utils/pattern_generator.py
+++ app/utils/pattern_generator.py
@@ -4,6 +4,14 @@ import plotly.graph_objs as go
 from .calculations import calculate_parameters
 from .config_loader import get_pseudo_dome_config
 
+rounding_decimal = 4
+
+def calculate_slope(x1, y1, x2, y2):
+    '''Calc slop, return Inf if horizontal'''
+    if x2 - x1 == 0:
+        return float('inf')  
+    else:
+        return (y2 - y1) / (x2 - x1)
 
 def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=None, mv_width=None, radial_width=None):
     # Load configuration from YAML file
@@ -38,8 +46,8 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
     def generate_half_pattern(inverse=False):
         current_x, current_y = 0, 0
         current_angle = 0
-        points = [(current_x, current_y)]
-        
+        points = []
+        starts_of_radial_segments = []
         for i in range(len(s)):
             # Calculate end point of current segment
             angle_factor = -1 if inverse else 1
@@ -51,11 +59,14 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
                 color = fold_color_2
             else:
                 color = fold_color_1
+            
+            if (i==0) & (current_angle==0):
+                starts_of_radial_segments.append((next_x, next_y))
+            
             traces.append(go.Scatter(x=[current_x, next_x], y=[current_y, next_y],
                                      mode='lines', line=dict(color=color, width=mv_width)))
             
             current_x, current_y = next_x, next_y
-            points.append((current_x, current_y))
             
             # Update angle for next segment
             if i < len(s) - 1:
@@ -65,7 +76,10 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
                     current_angle -= np.pi - beta[i]
                 else:
                     current_angle += np.pi - beta[i]
-
+        points.append((current_x, current_y))
+        starts_of_radial_segments.append((0, 0))
+        if (n%2==0):
+            starts_of_radial_segments = starts_of_radial_segments[::-1]
         # Add last segment with corrected angle calculation
         if inverse:
             if i % 2 == 1:
@@ -90,13 +104,13 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
 
         # Generate radial lines
         
-        for i in range(1, len(points)):
+        for i in range(0, len(points)):
             # Apply line style based on configuration
             line_dash = None
             if radial_line_style in ('dash', 'dot', 'dashdot'):
                 line_dash = radial_line_style
                 
-            traces.append(go.Scatter(x=[0, points[i][0]], y=[0, points[i][1]],
+            traces.append(go.Scatter(x=[starts_of_radial_segments[i][0], points[i][0]], y=[starts_of_radial_segments[i][1], points[i][1]],
                                    mode='lines', line=dict(color=radial_color, width=radial_width, dash=line_dash)))
 
     
@@ -108,7 +122,10 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
     unique_traces = []
     unique_coords = set()
     for i, trace in enumerate(traces):
-        trace_coords = (trace['x'], trace['y'])
+        trace_coords = (np.round(trace['x'][0], rounding_decimal),
+                        np.round(trace['y'][0], rounding_decimal),
+                        np.round(trace['x'][-1], rounding_decimal),
+                        np.round(trace['y'][-1], rounding_decimal))
         if trace_coords not in unique_coords:
             unique_coords.add(trace_coords)
             unique_traces.append(trace)
@@ -117,8 +134,8 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
     full_traces = traces.copy()
     
     # import pdb; pdb.set_trace()
-    for i in range(1, int(num_radial_segments/2)):
-    # for i in range(0,1):
+    # for i in range(1, int(num_radial_segments/2)):
+    for i in range(0,2):
         rotation = i * 2*alpha[0][0]
         rot_matrix = np.array([[np.cos(rotation), -np.sin(rotation)],
                                [np.sin(rotation), np.cos(rotation)]])
@@ -131,10 +148,18 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
                                         line=trace['line'] if 'line' in trace else None,
                                         marker=trace['marker'] if 'marker' in trace else None))
 
-            # if i == int(num_radial_segments/2)-1:
-            #     if trace['mode'] == 'marker':
-            #         first_and_last_markers.append((rotated_points[x][0],rotated_points[y][0]))
-    # print(first_and_last_markers)
+    # Remove duplicate traces
+    unique_traces = []
+    unique_coords = set()
+    for i, trace in enumerate(full_traces):
+        trace_coords = (np.round(trace['x'][0], rounding_decimal),
+                        np.round(trace['y'][0], rounding_decimal),
+                        np.round(trace['x'][-1], rounding_decimal),
+                        np.round(trace['y'][-1], rounding_decimal))
+        if trace_coords not in unique_coords:
+            unique_coords.add(trace_coords)
+            unique_traces.append(trace)
+    full_traces = unique_traces
     # add a cut line to the end
     # find the trace with ending point closest to positive x axis
     closest_trace = None
@@ -157,12 +182,16 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
         # cutline coordinates
         cutline_xpositions = (0, closest_trace['x'][-1])
         cutline_ypositions = (0, closest_trace['y'][-1])
+        cutline_slope = np.round(calculate_slope(cutline_xpositions[0], cutline_ypositions[0], cutline_xpositions[1], cutline_ypositions[1]), rounding_decimal)
 
-        # find and remove trace that has the same endpoints as the cutline
+        # find and remove trace that has the same endpoints as the cutline & is parallel to it
         for i, trace in enumerate(full_traces):
-            if (tuple(trace['x']) == cutline_xpositions) and (tuple(trace['y']) == cutline_ypositions):
+            trace_slope = np.round(calculate_slope(trace['x'][0], trace['y'][0], trace['x'][1], trace['y'][1]), rounding_decimal)
+            if (np.round(trace['x'][1], rounding_decimal) == np.round(cutline_xpositions[1], rounding_decimal)) and\
+               (np.round(trace['y'][1], rounding_decimal) == np.round(cutline_ypositions[1], rounding_decimal)) and\
+               (trace_slope ==cutline_slope):
                 full_traces.pop(i)
-                break
+                # break
         # Add a cut line from origin to the endpoint of the trace closest to positive x-axis
         full_traces.append(go.Scatter(
             x=cutline_xpositions, 
@@ -170,4 +199,5 @@ def generate_pattern(r, n, fold_color_1=None, fold_color_2=None, radial_color=No
             mode='lines', 
             line=dict(color=fold_color_1, width=mv_width)
         ))
+
     return full_traces
